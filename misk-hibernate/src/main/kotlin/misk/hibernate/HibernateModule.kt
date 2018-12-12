bff2508f2670be262ead99201b11f9d544b3a16e
Jon Tirsen <jontirsen@squareup.com>
2018-12-12 11:55:38 +0100

    Start Vitess when starting server in DEVELOPMENT environment

--- misk-hibernate/src/main/kotlin/misk/hibernate/HibernateModule.kt
+++ misk-hibernate/src/main/kotlin/misk/hibernate/HibernateModule.kt
@@ -12,6 +12,7 @@ import misk.jdbc.DataSourceDecorator
 import misk.jdbc.DataSourceService
 import misk.jdbc.PingDatabaseService
 import misk.resources.ResourceLoader
+import misk.vitess.StartVitessService
 import org.hibernate.SessionFactory
 import org.hibernate.event.spi.EventType
 import javax.inject.Inject
@@ -65,6 +66,8 @@ class HibernateModule(
     val dataSourceProvider = getProvider(dataSourceKey)
 
     val dataSourceServiceKey = DataSourceService::class.toKey(qualifier)
+
+    maybeBindStartVitessService()
 
     bind(configKey).toInstance(config)
 
@@ -120,5 +123,16 @@ class HibernateModule(
     })
 
     install(HibernateHealthCheckModule(qualifier, sessionFactoryProvider, config))
+  }
+
+  private fun maybeBindStartVitessService() {
+    val environment = Environment.fromEnvironmentVariable()
+    if (environment == Environment.DEVELOPMENT || environment == Environment.TESTING) {
+      val startVitessServiceKey = StartVitessService::class.toKey(qualifier)
+      multibind<Service>().to(startVitessServiceKey)
+      bind(startVitessServiceKey).toProvider(Provider<StartVitessService> {
+        StartVitessService(environment = environment, config = config, qualifier = qualifier)
+      }).asSingleton()
+    }
   }
 }
